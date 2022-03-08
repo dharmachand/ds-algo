0e4a144c10ac058b62eb16b234c635e841500ab7
Chand Bhaverisetti <dharmachand@gmail.com>
2022-03-07 21:27:18 -0800

    fixing ReverseWords

--- src/misc/ReverseWords.java
+++ src/misc/ReverseWords.java
@@ -1,21 +1,28 @@
 package misc;
 
 public class ReverseWords {
+
+    public static final char BLANK = ' ';
+
     public static void main(String[] args) {
-        char[] a = {'H', 'e', 'l', 'l', 'o', 'o',' ',
-                'J', 'a', 'v', 'a', ' ',
+        char[] a = {'H', 'e', 'l', 'l', 'o', 'o', BLANK,
+                'J', 'a', 'v', 'a', BLANK,
                 'W', 'o', 'r', 'l', 'd'};
         //char[] a = {'H', 'e', 'l', 'l', 'o', 'o'};
         System.out.println(String.valueOf(a));
 
         int n = a.length;
+        // no need to reverse if sentence has single word
+        boolean hasBlanks = hasBlanks(a);
+        if (!hasBlanks) return;
+
         // reverse whole sentence array
         reverse(a, 0, n - 1);
 
         //reverse each word in sentence array
         int ws = 0, we = 0;
         for (int i = 0; i < n; i++) {
-            if (a[i] == ' ') {
+            if (a[i] == BLANK) {
                 we = i - 1;
                 reverse(a, ws, we);
                 ws = i + 1;
@@ -24,6 +31,17 @@ public class ReverseWords {
         if (ws > 0) reverse(a, ws, n - 1);
 
         System.out.println(String.valueOf(a));
+    }
+
+    private static boolean hasBlanks(char[] a) {
+        boolean hasBlank = false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == BLANK) {
+                hasBlank = true;
+                break;
+            }
+        }
+        return hasBlank;
     }
 
 
